b7fe655e669ae21c1c351c417f00eb0cc7dde59e
Mher Ghukasyan <79404846+mhergh4@users.noreply.github.com>
2023-11-07 12:42:32 +0400

    add viginere method code

--- main.cpp
+++ main.cpp
@@ -1,29 +1,51 @@
 #include <iostream>
-#include "RLE2.h"
+#include <cctype>
+#include <string>
+
+std::string encode(const std::string& plaintext, const std::string& key) {
+    std::string encoded;
+    for (int i{ 0 };i < plaintext.size();++i) {
+        if (isalpha(plaintext[i])) {
+            char encodedChar = 'a' + ((std::tolower(plaintext[i]) - 'a') + (std::tolower(key[i % key.size()]) - 'a')) % 26;
+            if (std::isupper(plaintext[i])) {
+                encodedChar = std::toupper(encodedChar);
+            }
+
+            encoded.push_back(encodedChar);
+        }
+        else {
+            encoded.push_back(plaintext[i]);
+        }
+    }
+
+    return encoded;
+}
+
+std::string decode(const std::string& ciphertext, const std::string& key) {
+    std::string decoded;
+    for (int i{ 0 };i < ciphertext.size();++i) {
+        if (isalpha(ciphertext[i])) {
+            char decodedChar = 'a' + ((std::tolower(ciphertext[i]) - 'a') - (std::tolower(key[(i % key.size())]) - 'a') + 26) % 26;
+            if (isupper(ciphertext[i])) {
+                decodedChar = std::toupper(decodedChar);
+            }
+
+            decoded.push_back(decodedChar);
+        }
+        else {
+            decoded.push_back(ciphertext[i]);
+        }
+    }
+
+    return decoded;
+}
+
 int main() {
-    /*ParzPoxarinum pp;
-
-    std::cout << "Alphabets are:\n" << pp.alphabet << "\n" << pp.alphabet2 << std::endl;
-
-    std::string str;
-    std::cout << "Enter the string" << std::endl;
-    std::getline(std::cin, str);
-
-    pp.encodeDecode(str);
-
-    return 0;*/
-    /*std::string key = "COLUMNROW";
-    EncColumnsAndRows encryptor(key);
-    std::string plaintext = "HELLOWORLD";
-
-    encryptor.encodeDecode(plaintext);
-
-    return 0;*/
-    RLE2 rle;
-    std::string input;
-    std::cout << "Enter the text: ";
-    std::getline(std::cin, input);
-    std::cout << std::endl;
-    rle.encodeDecode(input);
-
+    std::string plaintxt = "Hello my name is Mher";
+    std::string key = "Lemon";
+    std::string result = encode(plaintxt, key);
+    std::cout << "Plaintext: " << plaintxt;
+    std::cout << "\nEncoded: " << result;
+    result = decode(result, key);
+    std::cout << "\nDecoded: " << result;
 }
